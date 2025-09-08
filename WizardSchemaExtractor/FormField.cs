e670ce49fe0c934d12c91857446edd3862c95045
Gaetan Tauvette <gaetan.tauvette@tc.gc.ca>
2025-09-08 15:09:31 -0400

    Removed JSON attributes.

--- WizardSchemaExtractor/FormField.cs
+++ WizardSchemaExtractor/FormField.cs
@@ -1,23 +1,13 @@
-using System.Text.Json.Serialization;
-
 namespace WizardSchemaExtractor;
 
 public class FormField
 {
-    [JsonPropertyName("index")]
     public int Index { get; set; }
-    [JsonPropertyName("tag")]
     public string Tag { get; set; } = "";
-    [JsonPropertyName("type")]
     public string Type { get; set; } = "";
-    [JsonPropertyName("id")]
     public string Id { get; set; } = "";
-    [JsonPropertyName("name")]
     public string Name { get; set; } = "";
-    [JsonPropertyName("placeholder")]
     public string Placeholder { get; set; } = "";
-    [JsonPropertyName("labelText")]
     public string LabelText { get; set; } = "";
-    [JsonPropertyName("value")]
     public string? Value { get; set; }
 }
